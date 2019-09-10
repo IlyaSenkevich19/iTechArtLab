35a61f8d7f5a7cb9c001afd244f3c61f3820e9e2
Ilya Senkevich <Ilya.Senkevich@itechart-group.com>
2019-09-10 17:49:58 +0300

    fix

--- task2/src/main.js
+++ task2/src/main.js
@@ -95,23 +95,7 @@ const reverseDate = findElement('.reverseDate');
 const secondRevDate = findElement('.secondRevDate');
 const dateFromNow = findElement('.dateFromNow');
 
-const li = document.querySelectorAll('li');
 
-shortDate.addEventListener('click', () => {
-    li[0].innerHTML = `"31102011"=>"${dateFormatter.shortDate('31102011')}"`
-})
-longDate.addEventListener('click', () => {
-    li[1].innerHTML = `"31102011" => "${dateFormatter.longDate('31102011')}"`
-})
-reverseDate.addEventListener('click', () => {
-    li[2].innerHTML = `("20130431", "YYYYMMDD") => ${dateFormatter.reverseDate('20130431', 'YYYYMMDD')} `
-})
-secondRevDate.addEventListener('click', () => {
-    li[3].innerHTML = `("20130431", "YYYYMMDD", "MM-DD-YYYY") => ${dateFormatter.secondReverseDate('20130431', 'YYYYMMDD', 'MM-DD-YYYY')} `
-})
-dateFromNow.addEventListener('click', () => {
-    li[4].innerHTML = `("2013-04-31", "YYYY-MM-DD").fromNow() => ${dateFormatter.dateFromNow('2013-04-31', 'YYYY-MM-DD')} `
-})
 
 //stringCalculator
 
